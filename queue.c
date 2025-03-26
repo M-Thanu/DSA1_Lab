aa0273a03d0234218a4cee235dd31c9626f19121
M-Thanu <mahanthanuja@gmail.com>
2025-03-26 11:31:08 +0530

    queue done

--- queue.c
+++ queue.c
@@ -6,32 +6,96 @@ struct node{
     struct node* next;
 };
 
-int main(){
-    struct node*front, *new;
-    struct node*temp = NULL;
-    front=(struct node*)malloc(sizeof(struct node));
+struct node*front, *rear;
 
-    int num;
-    printf("ENter the number of elements: ");
-    scanf("%d",&num);
-    int d;
+void enqueue(int num){
+    struct node* n;
+    n=(struct node*)malloc(sizeof(struct node));
+    n->data=num;
+    n->next=NULL;
 
-    for(int i=0; i<num; i++){
-        new=(struct node*)malloc(sizeof(struct node));
-        printf("Enter the data: ");
-        scanf("%d",&d);
-        new->data=d;
-        new->next=NULL;
-
-        if(front==NULL){
-            front=new;
-            temp=front;
-        }else{
-            temp->next=temp;
-            temp=new;
-            
-        }
-        
+    if(front==NULL && rear==NULL){
+        front=rear=n;
+    }else{
+        rear->next=n;
+        rear=n;
     }
+}
+
+void dequeue(){
+    struct node* t;
+    t=front;
+    front=front->next;
+    if(front==NULL){
+        rear=NULL;
+    }
+    free(t);
+}
+
+void peek(){
+    printf("%d",front->data);
+}
+
+void display(){
+    struct node* t;
+    t=front;
+    while(t!=NULL){
+        printf("%d\t",t->data);
+        t=t->next;
+    }
+}
+
+void isEmpty(){
+    if(front==NULL){
+        printf("Queue is empty");
+    }else{
+        printf("Queue is not empty");
+    }
+
+}
+void isFull(){
+    printf("Not Defined as total memory space is not assigned");
+
+}
+
+int main(){
+
+   
+    while(1){
+        printf("\n");
+        int ch;
+        printf("Menu\n1.Enqueue\n2.Dequeue\n3.Peek\n4.isEmpty\n5.isFull\n6.Display\n7.Exit");
+        printf("\nENter the choice: ");
+        scanf("%d",&ch);
+        int d;
+        switch(ch){
+            case 1:
+                printf("Enter the data: ");
+                scanf("%d",&d);
+                enqueue(d);
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                peek();
+                break;
+            case 4:
+                isEmpty();
+                break;
+            case 5:
+                isFull();
+                break;
+            case 6:
+                display();
+                break;
+            case 7:
+                exit(0);
+            default:
+                printf("Enter correct choice");
+        }
+    }
+    
+   
     return 0;
 }
